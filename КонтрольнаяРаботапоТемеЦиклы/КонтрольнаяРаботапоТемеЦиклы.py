c714dc2eec4a93c7cc36c937cd8d9fdece6190e4
AleksandrAleksejev <sashaaleksejev813577@gmail.com>
2021-11-23 16:39:57 +0300

    е

--- КонтрольнаяРаботапоТемеЦиклы/КонтрольнаяРаботапоТемеЦиклы.py
+++ КонтрольнаяРаботапоТемеЦиклы/КонтрольнаяРаботапоТемеЦиклы.py
@@ -1,20 +1,22 @@
 #1
-n = int(input("Введите число скворечников от 1 до 9: "))
-for i in range(n):
-   ckv= [" ----- ",
-        "|  O  |",
-        "!  -  !",
-         " ----- "]
-   if n>9:
-       break
-for i in ckv:
-    print(i * n)
+#n = int(input("Введите число скворечников от 1 до 9: "))
+#for i in range(n):
+#   ckv= [" ----- ",
+#        "|  O  |",
+#        "!  -  !",
+#         " ----- "]
+#for i in ckv:
+#    print(i * n)
 #2
 #n = int(input("Введите число: "))
 #b = int(input("Введите показатель чисел: "))
 #for l in range(1,n+1):
 #   print(l**b,end=" ")
 #3
+from random import *
+opilane=randint(5,30)
+arv=randint(1,5)
+print(opilane)
 
 #4
 
